a57f2f7a1ec2e0308df4de6ab3dd6347c95e6c3d
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-31 19:48:06 +0530

    add depended resources

--- modules/fargate/fargate.tf
+++ modules/fargate/fargate.tf
@@ -68,6 +68,17 @@ resource "aws_lb" "app_load_balancer" {
   subnets            = data.aws_subnets.public.ids
 }
 
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
+
 resource "aws_lb_target_group" "app_target_group" {
   name     = "app-target-group"
   port     = 80
@@ -83,17 +94,8 @@ resource "aws_lb_target_group" "app_target_group" {
     healthy_threshold    = 2
     unhealthy_threshold  = 2
   }
-}
 
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.app_load_balancer.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_target_group.arn
-  }
+  depends_on = [aws_lb_listener.http_listener]
 }
 
 resource "aws_ecs_task_definition" "mario_task" {
