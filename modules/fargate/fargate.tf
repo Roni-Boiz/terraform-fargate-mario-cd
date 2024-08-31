82fb9f282892c49a03b49d00159ce5699c533e2c
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-31 19:23:23 +0530

    add target type: ip

--- modules/fargate/fargate.tf
+++ modules/fargate/fargate.tf
@@ -73,6 +73,7 @@ resource "aws_lb_target_group" "app_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
+  target_type = "ip"
 
   health_check {
     port                = "traffic-port"
